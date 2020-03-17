2155d45bda47c7da4510f02926500a1c4413ed74
rushitSoftrefine <rushit.softrefine@gmail.com>
2020-03-17 17:00:38 +0530

    + Extend PeopleListRow to display user images.

--- components/PeopleListRow.js
+++ components/PeopleListRow.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {View, Image, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 const s = StyleSheet.create({
   root: {
+    alignItems: 'center',
+    flexDirection: 'row',
     padding: 10,
     paddingHorizontal: 10,
     marginHorizontal: 10,
     borderBottomColor: '#000',
     borderBottomWidth: StyleSheet.hairlineWidth,
   },
+  userImage: {
+    width: 40,
+    height: 40,
+    marginLeft: 8,
+    marginRight: 16,
+    marginTop: 8,
+    marginBottom: 8,
+  },
 });
 
-const PeopleListRow = (data) => {
+const PeopleListRow = data => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity activeOpacity={1}>
       <View style={s.root}>
-        <Text children={`${data.name.first}`} />{/* Fix PeopleListRow to display names correctly.  */}
+        <Image source={{uri: data.picture.medium}} style={s.userImage} />{/* Extend PeopleListRow to display user images. */}
+        <Text children={`${data.name.first + ' ' + data.name.last}`} />{/* Fix PeopleListRow to display names correctly. */}
       </View>
     </TouchableOpacity>
   );
